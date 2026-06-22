7dbcbaf38b2a36868eec81d73c50a0a70bbc90c2
jose-barroso <carlosbarroso2000@gmail.com>
2026-06-22 13:24:03 +0100

    MOCPErrorListener,suporte a erros do ANTLR

--- src/main/java/mocp/MOCPErrorListener.java
+++ src/main/java/mocp/MOCPErrorListener.java
@@ -9,6 +9,7 @@ import org.antlr.v4.runtime.Token;
 public class MOCPErrorListener extends BaseErrorListener {
 
   private int numErros = 0;
+  private boolean silenciarCascata = false; // NOVA FLAG: O nosso silenciador
 
   @Override
   public void syntaxError(Recognizer<?, ?> recognizer,
@@ -16,28 +17,46 @@ public class MOCPErrorListener extends BaseErrorListener {
                           int line, int charPositionInLine,
                           String msg,
                           RecognitionException e) {
+
+    // Se já detetámos um erro estrutural grave, ignoramos os erros de "pânico" do ANTLR
+    if (silenciarCascata) {
+      return;
+    }
+
     numErros++;
 
-    // 1. Se a mensagem contiver "token recognition error", é um erro léxico genérico (ex: caracteres não reconhecidos)
+    // 1. Se a mensagem contiver "token recognition error", é um erro léxico genérico
     if (msg != null && msg.contains("token recognition error")) {
       System.err.println("Erro léxico: " + msg + " na linha " + line + ", coluna " + charPositionInLine);
       return;
     }
 
-    // 2. NOVA VERIFICAÇÃO: Apanha o "Token Venenoso" das keywords de C
+    // 2. Apanha o "Token Venenoso" das keywords de C
     if (offendingSymbol instanceof Token) {
       Token token = (Token) offendingSymbol;
 
-      // Verifica se o token pertence à nossa regra ERR_C_KEYWORD
       if (token.getType() == MOCPLexer.ERR_C_KEYWORD) {
         System.err.println("[Erro Léxico/Sintático] Linha " + line + ", coluna " + charPositionInLine +
                 ": Palavra-chave de C '" + token.getText() +
                 "' não é permitida. Na linguagem MOCP utilize o equivalente em português!");
-        return; // Retorna para não imprimir a mensagem sintática genérica abaixo
+
+        // silenciador, daqui para a frente nenhum erro do parser é impresso.
+        silenciarCascata = true;
+        return;
       }
     }
 
-    // 3. Se não for nenhum dos casos acima, trata como um erro sintático normal
+    // 3. Apanha instruções soltas no escopo global
+    if (msg != null && msg.contains("expecting {<EOF>, 'inteiro', 'real', 'vazio'}")) {
+      System.err.println("[Erro Sintático] Linha " + line + ", coluna " + charPositionInLine +
+              ": Instrução solta inválida. No escopo global (fora de funções) apenas podes declarar protótipos, funções ou variáveis globais iniciadas por um tipo.");
+
+      // Ativamos o silenciador aqui também para evitar "lixo" no terminal
+      silenciarCascata = true;
+      return;
+    }
+
+    // 4. Trata como um erro sintático normal (fallback)
     System.err.println("Erro na linha " + line + ", coluna " + charPositionInLine + ": " + msg);
   }
 
