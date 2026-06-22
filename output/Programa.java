511f5de12d6431b8d068961c8015347b004b25e4
jose-barroso <carlosbarroso2000@gmail.com>
2026-06-22 02:49:56 +0100

    jogo da forca em mocp

--- output/Programa.java
+++ output/Programa.java
@@ -18,167 +18,116 @@ class Main {
     }
   }
 
-  private static int TAMANHO_MAX = 100;
-  private static double PI = 3.14159;
-  private static int contador = 0;
-  public static void cabecalho() {
-    System.out.print("===== PROGRAMA DE TESTE MOCP =====\n");
-  }
-
-  public static int fatorial(int n) {
-    if ((n <= 1)) {
-      return 1;
-    } else {
-      return (n * fatorial((n - 1)));
+  public static int calcular_tamanho(int[] str) {
+    int i = 0;
+    while ((str[i] != 0)) {
+      i = (i + 1);
     }
+    return i;
   }
 
-  public static double media(int[] v, int tamanho) {
+  public static void inicializar_escondida(int[] esc, int tam) {
     int i;
-    double soma = 0.0;
-    for (i = 0; (i < tamanho); i = (i + 1)) {
-      soma = (soma + (double)(v[i]));
+    for (i = 0; (i < tam); i = (i + 1)) {
+      esc[i] = '_';
     }
-    return (soma / (double)(tamanho));
+    esc[tam] = 0;
   }
 
-  public static int soma_vetor(int[] v, int tamanho) {
+  public static void limpar_ecra() {
     int i;
-    int soma = 0;
-    for (i = 0; (i < tamanho); i = (i + 1)) {
-      soma = (soma + v[i]);
+    for (i = 0; (i < 50); i = (i + 1)) {
+      System.out.print("\n");
     }
-    return soma;
   }
 
-  public static void escrever_vetor(int[] v, int tamanho) {
-    int i;
-    System.out.print("{");
-    for (i = 0; (i < tamanho); i = (i + 1)) {
-      System.out.print(v[i]);
-      if ((i < (tamanho - 1))) {
-        System.out.print(", ");
-      }
+  public static void desenhar_forca(int erros) {
+    System.out.print("\n +---+\n |   |\n");
+    if ((erros == 0)) {
+      System.out.print("     |\n     |\n     |\n");
     }
-    System.out.print("}\n");
-  }
-
-  public static int eh_primo(int n) {
-    int i;
-    if ((n <= 1)) {
-      return 0;
+    if ((erros == 1)) {
+      System.out.print(" O   |\n     |\n     |\n");
     }
-    if ((n == 2)) {
-      return 1;
+    if ((erros == 2)) {
+      System.out.print(" O   |\n |   |\n     |\n");
     }
-    if (((n % 2) == 0)) {
-      return 0;
+    if ((erros == 3)) {
+      System.out.print(" O   |\n/|   |\n     |\n");
     }
-    i = 3;
-    while (((i * i) <= n)) {
-      if (((n % i) == 0)) {
-        return 0;
-      }
-      i = (i + 2);
+    if ((erros == 4)) {
+      System.out.print(" O   |\n/|\\  |\n     |\n");
     }
-    return 1;
+    if ((erros == 5)) {
+      System.out.print(" O   |\n/|\\  |\n/    |\n");
+    }
+    if ((erros >= 6)) {
+      System.out.print(" O   |\n/|\\  |\n/ \\  |\n");
+    }
+    System.out.print("=========\n");
   }
 
   public static void main(String[] args) {
-    cabecalho();
-    int n;
+    int[] secreta;
+    int[] escondida = new int[50];
+    int tamanho;
+    int erros;
+    int acertos;
+    int max_erros;
     int i;
-    int opcao;
-    int[] v = new int[10];
-    double r;
-    int carac;
-    int[] vetor_lido;
-    int tamanho_lido = 0;
-    int a = -5;
-    double b = ((double)(-a) * 2.5);
-    System.out.print("Teste cast/unário: a = ");
-    System.out.print(a);
-    System.out.print(", b = ");
-    System.out.print(b);
-    System.out.print("\n");
-    System.out.print("Digite um caractere: ");
-    carac = scanner.next().charAt(0);
-    System.out.print((char)(carac));
-    System.out.print("\n");
-    System.out.print("Digite uma string (máx 10): ");
-    vetor_lido = lerStringParaVetor(scanner.next());
-    tamanho_lido = 0;
-    while ((vetor_lido[tamanho_lido] != 0)) {
-      tamanho_lido = (tamanho_lido + 1);
-    }
-    System.out.print("String lida: ");
-    imprimirStringVetor(vetor_lido);
-    System.out.print("\n");
-    n = scanner.nextInt();
-    n = 5;
-    for (i = 0; (i < n); i = (i + 1)) {
-      v[i] = ((i * 2) + 1);
-    }
-    int soma = soma_vetor(v, n);
-    double med = media(v, n);
-    System.out.print("Vetor: ");
-    escrever_vetor(v, n);
-    System.out.print("Soma = ");
-    System.out.print(soma);
-    System.out.print("\n");
-    System.out.print("Média = ");
-    System.out.print(med);
-    System.out.print("\n");
-    System.out.print("Fatorial de 5: ");
-    System.out.print(fatorial(5));
-    System.out.print("\n");
-    System.out.print("Números primos até 20: ");
-    for (i = 2; (i <= 20); i = (i + 1)) {
-      if ((eh_primo(i) == 1)) {
-        System.out.print(i);
-        System.out.print(" ");
+    int letra;
+    int acertou_nesta_ronda;
+    erros = 0;
+    acertos = 0;
+    max_erros = 6;
+    System.out.print("==================================\n");
+    System.out.print("    BEM-VINDO AO JOGO DA FORCA    \n");
+    System.out.print("==================================\n");
+    System.out.print("[JOGADOR 1] Introduz a palavra secreta: ");
+    secreta = lerStringParaVetor(scanner.next());
+    tamanho = calcular_tamanho(secreta);
+    inicializar_escondida(escondida, tamanho);
+    limpar_ecra();
+    System.out.print("=== O JOGO COMECOU! ===\n");
+    System.out.print("Dica: Usa apenas letras minusculas.\n");
+    while (((erros < max_erros) && (acertos < tamanho))) {
+      desenhar_forca(erros);
+      System.out.print("\nPalavra: ");
+      imprimirStringVetor(escondida);
+      System.out.print("\n");
+      System.out.print("Erros: ");
+      System.out.print(erros);
+      System.out.print("/");
+      System.out.print(max_erros);
+      System.out.print("\n");
+      System.out.print("\n[JOGADOR 2] Tenta uma letra: ");
+      letra = scanner.next().charAt(0);
+      acertou_nesta_ronda = 0;
+      for (i = 0; (i < tamanho); i = (i + 1)) {
+        if (((secreta[i] == letra) && (escondida[i] == '_'))) {
+          escondida[i] = letra;
+          acertos = (acertos + 1);
+          acertou_nesta_ronda = 1;
+        }
+      }
+      if ((acertou_nesta_ronda == 0)) {
+        erros = (erros + 1);
       }
     }
-    System.out.print("\n");
-    int x = 10;
-    int y = 20;
-    if (((x > 0) && (y > 0))) {
-      System.out.print("Ambos positivos\n");
-    } else {
-      System.out.print("Algum não é positivo\n");
-    }
-    int count = 0;
-    while ((count < 3)) {
-      System.out.print("count = ");
-      System.out.print(count);
+    limpar_ecra();
+    desenhar_forca(erros);
+    if ((acertos == tamanho)) {
+      System.out.print("\n[VITORIA] PARABENS! Tu salvaste o boneco!\n");
+      System.out.print("A palavra era: ");
+      imprimirStringVetor(secreta);
       System.out.print("\n");
-      count = (count + 1);
-    }
-    for (i = 0; (i < 5); i = (i + 1)) {
-      v[i] = (v[i] * 2);
-    }
-    System.out.print("Vetor após duplicar: ");
-    escrever_vetor(v, n);
-    double pi = PI;
-    int pi_int = (int)(pi);
-    System.out.print("PI = ");
-    System.out.print(pi);
-    System.out.print(", PI como inteiro = ");
-    System.out.print(pi_int);
-    System.out.print("\n");
-    int flag = 1;
-    if ((flag == 0)) {
-      System.out.print("Flag é falso\n");
     } else {
-      System.out.print("Flag é verdadeiro\n");
+      System.out.print("\n[DERROTA] GAME OVER! O boneco foi enforcado.\n");
+      System.out.print("A palavra secreta era: ");
+      imprimirStringVetor(secreta);
+      System.out.print("\n");
     }
-    System.out.print("v[3] = ");
-    System.out.print(v[3]);
-    System.out.print("\n");
-    int c = 65;
-    System.out.print((char)(c));
-    System.out.print("\n");
-    System.out.print("Fim do programa!\n");
+    System.exit(0);
   }
 
 }
